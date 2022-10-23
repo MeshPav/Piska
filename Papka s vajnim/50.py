55c40935c71c2d809153f49cd6e08a89ef16c732
RolonStewart <115928623+RolonStewart@users.noreply.github.com>
2022-10-23 21:23:19 +1000

    better

--- Papka s vajnim/50.py
+++ Papka s vajnim/50.py
@@ -1,27 +1,20 @@
-x = float(input('Введите  магнитуду землятресения: '))
-if x == < 2:
-    print('Минимальное')
-
-elif x == >= 2 and x == < 3:
-    print('Очень слабое')
-
-elif x == >= 3 and x == < 4:
-    print('Слабое')
-
-elif x == >= 4 and x == < 5:
-    print('Промежуточное')
-
-elif x == >= 5 and x == < 6
-    print('Умеренное')
-
-elif x == >= 6 and x == < 7:
-    print('Сильное!')
-
-elif x == >= 7 and x == < 8:
-    print('Очень сильное!!')
-
-elif x == >= 8 and x == < 10:
-    print('Огромное!!!')
-
-else x == >= 10:
-    print('Разрушительное!!!!')
+while True:
+    x = int(input('Введите  магнитуду землятресения: '))
+    if x <= 2:
+        print('Минимальное')
+    elif x <= 3:
+        print('Очень слабое')
+    elif x <= 4:
+        print('Слабое')
+    elif x <= 5:
+        print('Промежуточное')
+    elif x <= 6:
+        print('Умеренное')
+    elif x <= 7:
+        print('Сильное!')
+    elif x <= 8:
+        print('Очень сильное!!')
+    elif x <= 10:
+        print('Огромное!!!')
+    elif x > 10:
+        print('Разрушительное!!!!')
